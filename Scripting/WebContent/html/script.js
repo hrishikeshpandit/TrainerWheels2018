f841bbce4c04eaa9404cb05e01821acc495d6d3e
Hrishi <hpandit398@gmail.com>
2018-09-06 16:07:56 +0530

    Exercise and Assignments included

--- Scripting/WebContent/html/script.js
+++ Scripting/WebContent/html/script.js
@@ -46,3 +46,69 @@ function dynamic(){
 	console.log(myNewCar2);
 	alert(myNewCar2.display());
 }
+
+
+
+function bookstore(){
+	function book(title,author,read){
+		this.title=title;
+		this.author=author;
+		this.read=read;
+	}
+	var shelf= [];
+	shelf.push(new book("kmes","Me",0))
+	shelf.push(new book("Jdff","le",0))
+		shelf.push(new book("Jfes","lke",1))
+     for(i =0;i<3;i++){
+    	 console.log(shelf[i].title);
+    	 console.log(shelf[i].author);
+    	 if(shelf[i].read==0){
+    		 console.log("NOT read");
+    	 }else{
+    		 console.log("read");
+    	 }
+     }
+}
+
+function movieTheater(){
+	function movie(title,duration,actors){
+		this.title=title;
+		this.duration=duration;
+		this.actors=actors;
+		this.display=function(){
+			var result="the movie name is "+this.title+" and the duration is "+duration+ " the actors are  "+actors[0]+" ,"+actors[1];
+		    return result;	
+		}
+	}
+	var mov=[];
+	mov.push(new movie("MI6",45,Array("Tom","Rob")));
+	mov.push(new movie("MI5",45,Array("Tom","Robin")));
+	for(i=0;i<mov.length;i++){
+		console.log(mov[i]);
+		console.log("the movie name");
+		console.log(mov[i].display());
+	}
+
+}
+
+
+function shoppingCart(){
+	var cart1={
+			  banana: "1.25",
+			  handkerchief: ".99",
+			  Tshirt: "25.01",
+			  apple: "0.60",
+			  nalgene: "10.34",
+			  proteinShake: "22.36"			
+				}
+	var x;
+	var val=0;
+	function cartel(obj){
+		for( x in obj){
+			val=val+parseFloat(obj[x]);
+			console.log(val);
+		}
+		alert("Your Total is "+val+" rupees");
+	}
+	cartel(cart1);
+}
